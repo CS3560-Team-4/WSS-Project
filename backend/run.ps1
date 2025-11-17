12d01dcd7a57092bde31c36ef089883e563860c3
Dale Peligro <ddparmstrong2021@gmail.com>
2025-11-16 20:06:39 -0800

    altered run.ps1

--- backend/run.ps1
+++ backend/run.ps1
@@ -1,19 +1,6 @@
-$ErrorActionPreference = "Stop"
+Write-Host "Cleaning and compiling backend..."
+.\mvnw.cmd clean compile
 
-Write-Host  "Cleaning and compiling backend..." -ForegroundColor Cyan
-
-if (Get-Command mvn -ErrorAction SilentlyContinue) {
-    mvn clean compile
-    Write-Host "Running backend..." -ForegroundColor Green
-    mvn exec:java
-}
-elseif (Get-Command wsl -ErrorAction SilentlyContinue) {
-    Write-Host "⚙ Using Maven to run backend via WSL..." -ForegroundColor Yellow
-    wsl mvn clean compile
-    wsl mvn exec:java
-}
-else {
-    Write-Host "❌ Neither Maven nor WSL detected. Please install Maven or WSL." -ForegroundColor Red
-    exit 1
-}
+Write-Host "Running Java backend..."
+.\mvnw.cmd exec:java
 
