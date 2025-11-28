da82c7a4959e0d87ce12dc01a2951022b9ef8b5b
Dale Peligro <ddparmstrong2021@gmail.com>
2025-11-28 00:16:30 -0800

    updated welcome screen typo

--- frontend/src/components/WelcomeScreen.jsx
+++ frontend/src/components/WelcomeScreen.jsx
@@ -1,18 +1,16 @@
 const WelcomeScreen = ({ onStart }) => {
   return (
     <div className="flex flex-col items-center justify-center text-center p-6">
-      <div className="text-4xl font-bold mb-4 text-white">
-        Welcome to WSS
-      </div>
+      <div className="text-4xl font-bold mb-4 text-white">Welcome to WSS</div>
 
       <p className="text-lg text-gray-300 mb-5 max-w-xs">
-        Navigate through the terrain and use as little resources
-        as possible to get to the end. 
+        Navigate through the terrain and use as little resources as possible to
+        get to the end.
       </p>
       <p className="text-lg text-gray-300 mb-5 max-w-xs">
-        Pick up some times along the way
-        to assist in your journey. Don't forget to stop by the traders as well to
-        exchange your gold for items. But be weary, they might try and take your money!
+        Pick up some items along the way to assist in your journey. Don't forget
+        to stop by the traders as well to exchange your gold for items. But be
+        weary, they might try and take your money!
       </p>
       <p className="text-lg text-gray-300 mb-6 max-w-xs">
         Are you ready for the challenge?
@@ -27,7 +25,7 @@ const WelcomeScreen = ({ onStart }) => {
         Start Game
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default WelcomeScreen;
