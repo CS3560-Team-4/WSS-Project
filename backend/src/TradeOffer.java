fd9b2c292da39b3925de659459855307531b3416
anthony <anthonymendoza3867@gmail.com>
2025-12-11 14:39:33 -0800

    comments

--- backend/src/TradeOffer.java
+++ backend/src/TradeOffer.java
@@ -1,14 +1,16 @@
 public class TradeOffer {
+    //resources that are given to player
     public int offerGold;
     public int offerWater;
     public int offerFood;
+    //resources trader wants
     public int requestGold;
     public int requestWater;
     public int requestFood;
 
     public Item offeredItem;
     public int goldCost;
-
+    //trade resource constructor
     public TradeOffer(int og, int ow, int of, int rg, int rw, int rf) {
         offerGold = og;
         offerWater = ow;
@@ -17,7 +19,7 @@ public class TradeOffer {
         requestWater = rw;
         requestFood = rf;
     }
-
+    //gold trade constructor
     public TradeOffer(Item item, int goldCost) {
         this.offeredItem = item;
         this.goldCost = goldCost;
--- backend/src/Trader.java
+++ backend/src/Trader.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 
 public class Trader {
+    //trader information
     protected String name;
     protected TraderType type;
     protected MoodState mood;
@@ -16,24 +17,24 @@ public class Trader {
         this.inTrade = false;
         this.previousOffer = null;
     }
-
+    //accepts player off
     public TradeOffer acceptOffer(TradeOffer playerOffer) {
         return playerOffer;
     }
-
+    //traders patience is decreased 
     public void decreasePatience() {
         patience--;
     }
-
+    //trade is rejected and ended
     public void rejectTrade() {
         inTrade = false;
     }
-
+    //starts new trade with player
     public void beginTrade(Player player) {
         inTrade = true;
         previousOffer = null;
     }
-
+    //check to see if trader is already in trade
     public boolean isInTrade() {
         return inTrade;
     }
@@ -41,12 +42,12 @@ public class Trader {
     public String toString() {
         return type.toString().toLowerCase();
     }
-
+    //new random offer is generated
     public TradeOffer generateOffer(Player player) {
         Item randomItem = new Item(
             ItemType.values()[new Random()
                 .nextInt(ItemType.values().length)]);
-        
+        //price dependant on traders mood
         int price = switch (this.type) {
             case Friendly -> 5;
             case Generous -> 3;
